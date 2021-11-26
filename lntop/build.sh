1f5480e665655f2fda9dd5ebe0a67083a70bd53b
Ioan Bizău <i@ibz.me>
2021-11-26 19:04:15 +0200

    v0.0.4

--- lntop/build.sh
+++ lntop/build.sh
@@ -3,19 +3,19 @@
 declare -a architectures=("amd64" "arm64")
 
 for architecture in "${architectures[@]}"; do
-  echo "Building for ${architecture}..."
-  docker buildx build --platform linux/${architecture} --tag ibz0/lntop-${architecture} --output "type=registry" --build-arg arch=${architecture} .
+  echo "Building $1 for ${architecture}..."
+  docker buildx build --platform linux/${architecture} --tag ibz0/lntop-${architecture}:$1 --output "type=registry" --build-arg arch=${architecture} .
 done
 
 echo "Creating manifest list..."
 for architecture in "${architectures[@]}"; do
-  echo " ibz0/lntop-${architecture}"
-done | xargs docker manifest create ibz0/lntop
+  echo " ibz0/lntop-${architecture}:$1"
+done | xargs docker manifest create ibz0/lntop:$1
 
 for architecture in "${architectures[@]}"; do
   echo "Annotating manifest for ${architecture}..."
-  docker manifest annotate ibz0/lntop ibz0/lntop-${architecture} --arch ${architecture} --os linux
+  docker manifest annotate ibz0/lntop:$1 ibz0/lntop-${architecture}:$1 --arch ${architecture} --os linux
 done
 
 echo "Pushing manifest list..."
-docker manifest push --purge ibz0/lntop
+docker manifest push --purge ibz0/lntop:$1
