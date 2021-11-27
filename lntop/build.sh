e703e907c4cdc93b953a53b3b5fb493ead4d98ad
Ioan Bizău <i@ibz.me>
2021-11-27 04:28:49 +0200

    v0.0.6

--- lntop/build.sh
+++ lntop/build.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+VERSION=v0.0.6
+
 declare -a architectures=("amd64" "arm64")
 
 for architecture in "${architectures[@]}"; do
-  echo "Building $1 for ${architecture}..."
-  docker buildx build --platform linux/${architecture} --tag ibz0/lntop-${architecture}:$1 --output "type=registry" --build-arg arch=${architecture} .
+  echo "Building ${VERSION} for ${architecture}..."
+  docker buildx build --platform linux/${architecture} --tag ibz0/lntop-${architecture}:${VERSION} --output "type=registry" --build-arg arch=${architecture} .
 done
 
 echo "Creating manifest list..."
 for architecture in "${architectures[@]}"; do
-  echo " ibz0/lntop-${architecture}:$1"
-done | xargs docker manifest create ibz0/lntop:$1
+  echo " ibz0/lntop-${architecture}:${VERSION}"
+done | xargs docker manifest create ibz0/lntop:${VERSION}
 
 for architecture in "${architectures[@]}"; do
   echo "Annotating manifest for ${architecture}..."
-  docker manifest annotate ibz0/lntop:$1 ibz0/lntop-${architecture}:$1 --arch ${architecture} --os linux
+  docker manifest annotate ibz0/lntop:${VERSION} ibz0/lntop-${architecture}:${VERSION} --arch ${architecture} --os linux
 done
 
 echo "Pushing manifest list..."
-docker manifest push --purge ibz0/lntop:$1
+docker manifest push --purge ibz0/lntop:${VERSION}
