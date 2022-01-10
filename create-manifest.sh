7a22006d15c28c89f1b6a99eaaf6e66a523828ba
Aaron Dewes <aaron.dewes@protonmail.com>
2022-01-10 14:37:15 +0100

    Try to improve actions

---
+++ create-manifest.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME=${1}
+VERSION=${2}
+BASE_IMAGE=${3}
+VERSION_PREFIX="${VERSION}-${BASE_IMAGE}"
+FINAL_NAME="${FINAL_NAME}"
+declare -a architectures=("amd64" "arm64")
+
+for architecture in "${architectures[@]}"; do
+  echo "Pulling ${VERSION} for ${architecture}..."
+  docker pull ${FINAL_NAME}-${architecture}
+done
+
+echo "Creating manifest list..."
+for architecture in "${architectures[@]}"; do
+  echo " ${FINAL_NAME}-${architecture}"
+done | xargs docker manifest create ${FINAL_NAME}
+
+for architecture in "${architectures[@]}"; do
+  echo "Annotating manifest for ${architecture}..."
+  docker manifest annotate ${FINAL_NAME} ${FINAL_NAME}-${architecture} --arch ${architecture} --os linux
+done
+
+echo "Pushing manifest list..."
+docker manifest push --purge ${FINAL_NAME}
