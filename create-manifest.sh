a55af411b57bafd48f645062a9571d7e38051361
Aaron Dewes <aaron.dewes@protonmail.com>
2022-01-11 07:25:06 +0100

    Build for bullseye as well. Use GH actions for build. Add BOS.
    * Refactor builds
    
    * Fix build script
    
    * Fix build failure
    
    * More cleanups
    
    * Install ttyd from Debian directly
    
    * Actually add source to sources.list
    
    * Try more optimizations
    
    * Add buster builds
    
    * Add ttyd dependencies
    
    * Fix ttyd path
    
    * Fix permissions for buster build script
    
    * Update Dockerfile.buster
    
    * Add BOS, add support for BITCOIN_NETWORK env var
    
    * Try to split buster builds into separate workflow
    
    * Go back to grpc hack for buster
    
    * Fix user name (hopefully)
    
    * Try to improve actions
    
    * Fix image name for workflows
    
    * Fix buster Dockerfile
    
    My editor was glitching while writing this.
    
    * Reduce container size
    
    Clear apt cache & sources list
    Upgrade dependencies
    Avoid installing recommended dependencies
    
    * Try to improve manifest creation script
    
    * More improvements
    
    * Install missing dependency on Buster
    
    * Make Dockerfiles generated
    
    * Try to reduce the amount of duplicated code
    
    * Improve versioning
    
    A build for a branch/tag now tags the version directly as an alias to bullsee in addition to the -bullseye and -buster version
    
    * Fix Dockerfile generation
    
    * Don't cancel all builds if one fails
    
    Final manifest creation will be skipped anyway, but the working images will be build & pushed, and other failing images will produce logs too.
    
    * Fix a bug in the create-manifest.sh
    
    * Try a better BOS fix
    
    * Fix permissions for BOS
    
    * Add another missing dependency
    
    * Fix BOS path
    
    * Install tmux in final container

---
+++ create-manifest.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME=${1}
+VERSION=${2}
+BASE_IMAGE=${3}
+VERSION_PREFIX="${VERSION}-${BASE_IMAGE}"
+FINAL_NAME="${IMAGE_NAME}:${VERSION_PREFIX}"
+# Manifest name is "${IMAGE_NAME}:${VERSION}" if $4 is true, $FINAL_NAME otherwise
+if [[ "${4}" == "true" ]]; then
+    MANIFEST_NAME="${IMAGE_NAME}:${VERSION}"
+else
+    MANIFEST_NAME="${FINAL_NAME}"
+fi
+declare -a architectures=("amd64" "arm64")
+
+for architecture in "${architectures[@]}"; do
+  echo "Pulling ${VERSION} for ${architecture}..."
+  docker pull "${FINAL_NAME}-${architecture}"
+done
+
+echo "Creating manifest list..."
+for architecture in "${architectures[@]}"; do
+  echo " ${FINAL_NAME}-${architecture}"
+done | xargs docker manifest create "${MANIFEST_NAME}"
+
+for architecture in "${architectures[@]}"; do
+  echo "Annotating manifest for ${architecture}..."
+  docker manifest annotate "${MANIFEST_NAME}" "${FINAL_NAME}-${architecture}" --arch ${architecture} --os linux
+done
+
+echo "Pushing manifest list..."
+docker manifest push --purge ${MANIFEST_NAME}
+
+# If $4 isn't true and $BASE_IMAGE is bullseye, rerun this script with $4 set to true
+if [[ "${4}" != "true" && "${BASE_IMAGE}" == "bullseye-slim" ]]; then
+    ./create-manifest.sh "${IMAGE_NAME}" "${VERSION}" "${BASE_IMAGE}" "true"
+fi
