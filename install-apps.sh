eee83128ec028e63671835c8fe0b02ceaf062efe
Ioan Bizău <i@ibz.me>
2022-02-06 15:12:10 +0200

    No repo in final image. All binaries in /usr/local

--- install-apps.sh
+++ install-apps.sh
@@ -5,6 +5,3 @@ while IFS=read -r line; do
     printf "#!/bin/bash" > $FILENAME
     echo $line | cut -d " " -f2- >> $FILENAME
 done <<< `jq -r '.apps[] | "\(.name) \(.run)"' /build/apps/apps.json`
-
-chown -R lnshell:lnshell /home/lnshell/.local/bin
-chmod +x /home/lnshell/.local/bin/*
