b9224d3aed885ef946d8176ff3351135b2ed9b6c
Ioan Bizău <i@ibz.me>
2022-02-14 18:32:26 +0200

    Fix lsh_exec.sh PATH

--- lsh_exec.sh
+++ lsh_exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 lsh=$(docker ps | grep lightning-shell | awk '{print $1}')
-docker exec -it $lsh /home/lnshell/.local/bin/$@
+docker exec -e PATH=/home/lnshell/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin -it $lsh $@
