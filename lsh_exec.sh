e8079f0c50c7fe1cca6f6484d302fba3cffcf593
Ioan Bizău <i@ibz.me>
2022-02-28 16:32:46 +0200

    +lsh_exec_ni.sh

--- lsh_exec.sh
+++ lsh_exec.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+script_name=`basename $0`
+
+if [[ "$script_name" == *"_ni"* ]]; then # non-interactive
+    extra_param=
+else
+    extra_param="-it"
+fi
+
 lsh=$(docker ps | grep lightning-shell | awk '{print $1}')
-docker exec -e PATH=/home/lnshell/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin -it $lsh $@
+docker exec -e PATH=/home/lnshell/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $extra_param $lsh $@
--- start.sh
+++ start.sh
@@ -10,4 +10,10 @@ if [ ! -x /data/lsh_exec.sh ]; then
     chmod +x /data/lsh_exec.sh
 fi
 
+if [ ! -x /data/lsh_exec_ni.sh ]; then
+    cp /lsh_exec.sh /data/lsh_exec_ni.sh
+    chown lnshell:lnshell /data/lsh_exec_ni.sh
+    chmod +x /data/lsh_exec_ni.sh
+fi
+
 /usr/bin/tini /usr/local/bin/ttyd -- --credential ${USERNAME}:${APP_PASSWORD} bash
